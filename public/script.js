7be0d4f8b31ff34b6fee6e7eee30d9b507ba19aa
Aly-Tamboura <alytamboura7370@gmail.com>
2017-06-19 15:48:08 -0700

    added jquery

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log('connected')
+$(document).ready(function() {
+ $('body').append('<p>hello</p>')
+})
+
+var renderBoard() {
+	var board = [];
+  for(var i = 0; i < 8; i++) {
+  	for(var k = 0; k <=8; k++) {
+     var board.push('<span>1</span>');
+  	}
+  }
+}
