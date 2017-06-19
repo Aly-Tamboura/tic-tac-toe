1ff1a0ebf5d895130357ffeadebe43312c801f09
Aly-Tamboura <alytamboura7370@gmail.com>
2017-06-19 15:37:01 -0700

    removed extra index added script.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = process.ENV || 3000
 const xMove = require('./x')
 app.use(express.static('public'))
 
-app.use('/', express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.listen(port, function () {
   console.log('tic-tac-toe listening on port 3000!')
