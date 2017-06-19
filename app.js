78d69a7e7a3d03c30218d6a55404ee1884df52f2
Aly-Tamboura <alytamboura7370@gmail.com>
2017-06-19 15:24:02 -0700

    added public file and html index

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.ENV || 3000
 const xMove = require('./x')
+app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use('/', express.static('public'))
 
 app.listen(port, function () {
   console.log('tic-tac-toe listening on port 3000!')
